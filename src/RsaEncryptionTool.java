4a9d6161166f19550b27880da993b175e39f5c51
Max <26414444+maxinnes@users.noreply.github.com>
2021-12-28 23:48:38 +0000

    Check if key directory exists

--- src/RsaEncryptionTool.java
+++ src/RsaEncryptionTool.java
@@ -1,12 +1,16 @@
 import uk.ac.uos.gui.RsaHome;
 import uk.ac.uos.rsa.RsaKeyPairManager;
-
 import java.io.IOException;
-
-// TODO make sure to check it keys directory exists
+import java.nio.file.*;
 
 public class RsaEncryptionTool {
     public static void main(String[] args) throws IOException {
+        String keyDirectory = System.getProperty("user.dir")+"/keys";
+        Path keyPath = Path.of(keyDirectory);
+        if(Files.notExists(keyPath)){
+            Files.createDirectories(keyPath);
+        }
+
         RsaKeyPairManager rsaKeyPairManager = new RsaKeyPairManager("db1.json");
         RsaHome MainWindow = new RsaHome(rsaKeyPairManager);
     }
